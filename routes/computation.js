17b893f77771add1906057c8b37944fbe11e99dc
Syam kumar Allaparthi <syamkumar.allaparthi@gmail.com>
2022-10-26 18:09:55 -0500

    updating the compuation files with the new functio

--- routes/computation.js
+++ routes/computation.js
@@ -4,7 +4,12 @@ var router = express.Router();
 /* GET users listing. */
 router.get('/', function(req, res, next) {
     var val = Math.floor(Math.random() * 10);
-    res.render('computation', { title: 'Syam Allaparthi generate\'s a random value and apply a Math function', math_result:`Math.exp()  applied to ${val} is ${Math.exp(val)}`});
+    res.render('computation', { title: 'Syam Allaparthi generate\'s a random value and apply a Math function', 
+    math_result:`Math.exp()  applied to ${val} is ${Math.exp(val)}`,
+    math_results:`Math.expm1()  applied to ${val} is ${Math.expm1(val)}`,
+    math_resultss:`Math.atan()  applied to ${val} is ${Math.atan(val)}`    
+
+    });
 });
 
 module.exports = router;
